f98fd51bba2767284404c7b7db3632c405c5cccd
sergbvso <sergbvso@gmail.com>
2024-02-28 17:48:22 +0300

    Add sources

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/base64"
+	"log"
+	"net/http"
+
+	"github.com/skip2/go-qrcode"
+)
+
+var html_start string = `<!doctype html>
+<html>
+<head>
+<title>URL QR</title>
+<meta name="description" content="URL QR">
+</head>
+<body>
+<img src='data:image/png;base64, `
+var html_end string = `' />
+</body>
+</html>
+`
+
+type Handler struct{}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	reqUrl := req.RequestURI
+	png, _ := qrcode.Encode(reqUrl, qrcode.Medium, 512)
+	dst := make([]byte, base64.StdEncoding.EncodedLen(len(png)))
+	base64.StdEncoding.Encode(dst, png)
+	htmlFull := html_start + string(dst) + html_end
+	_, err := w.Write([]byte(string(htmlFull)))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func main() {
+	log.Println(http.ListenAndServe(":8080", &Handler{}))
+}
